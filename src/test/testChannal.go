96ac2c862c2763fdb081658e8b8a93b71fd534f4
bfyang5130 <bfyang5130@qq.com>
2016-06-13 15:42:05 +0800

    to pack easy

--- src/test/testChannal.go
+++ src/test/testChannal.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
+)
+
+var (
+	intcount int
+	isOut    chan bool
 )
 
 func main() {
+	//定义一个控制主线程退出的处理
+	intcount = 1
 
 	//定义一个信道
 	intChannel := make(chan int, 10)
@@ -23,20 +29,40 @@ func main() {
 		fmt.Println("receive:", v)
 	}
 	*/
-	time.Sleep(30 * time.Second)
+	//退出堵塞
+	<-isOut
 }
 
 func Product(queue chan<- int) {
 
-	for i := 0; i < 10000000; i++ {
+	for i := 0; i < 1000; i++ {
 		queue <- i
 		fmt.Println("send:", i)
 	}
 }
 
 func Consumer(queue <-chan int, j int) {
+	//用来做一下记录，看看线程是不是一直在运行的
+	//打开一个文件向里面写东西
+	//wf, err := os.OpenFile(`./testchan.txt`, os.O_APPEND, 0775)
+	//if err != nil {
+	//	return
+	//}
+
 	for {
 		v := <-queue
-		fmt.Println(fmt.Sprintf("receive%d:%d", j, v))
+		fmt.Println(`receive:`, v)
+		//_, err1 := io.WriteString(wf, fmt.Sprintf(`%d`, v)+"\n")
+		//if err1 != nil {
+		//	fmt.Println("can not write file")
+		//}
+	}
+	addCount()
+}
+
+func addCount() {
+	intcount++
+	if intcount == 10 {
+		isOut <- true
 	}
 }
