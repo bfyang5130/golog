4e581ba0c68ce6edd367026bc96aac8da4e8606f
bfyang5130 <bfyang5130@qq.com>
2016-06-17 16:14:53 +0800

    anewdir

--- src/logFit/newsplitetofitfile.go
+++ src/logFit/newsplitetofitfile.go
@@ -23,15 +23,13 @@ import (
 var (
 	w sync.WaitGroup
 
-	fcaches     = filecache.Handler{"/home/golangpath/src/logFit/cache", 43200}
-	logFileName = flag.String("log", "/home/golangpath/src/logFit/cServer.log", "Log file name")
-	fixconf     = goini.SetConfig(`/home/golangpath/src/logFit/config/config.ini`)
-	/**
+	//fcaches     = filecache.Handler{"/var/golang/src/logFit/cache", 43200}
+	//logFileName = flag.String("log", "/var/golang/src/logFit/cServer.log", "Log file name")
+	//fixconf     = goini.SetConfig(`/var/golang/src/logFit/config/config.ini`)
 
 	fcaches     = filecache.Handler{"./cache", 43200}
 	logFileName = flag.String("log", "./cServer.log", "Log file name")
 	fixconf     = goini.SetConfig(`./config/config.ini`)
-	*/
 )
 
 func main() {
--- src/myfunc/tools.go
+++ src/myfunc/tools.go
@@ -272,7 +272,7 @@ func Convertip_tiny(ip string) (ipCountry, ipProvince, ipCity string) {
 	ipdot := strings.Split(ip, `.`)
 	//fmt.Println(ipdot[0])
 	//读取IP二进制文件
-	fi, err := os.Open("/home/golangpath/src/logFit/source/tinyipdata.dat")
+	fi, err := os.Open("/var/golang/src/logFit/source/tinyipdata.dat")
 	//fi, err := os.Open("./source/tinyipdata.dat")
 
 	if err != nil {
